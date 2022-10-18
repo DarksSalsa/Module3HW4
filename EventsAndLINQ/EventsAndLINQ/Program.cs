3240acc3da9dab250753f3b0e108d0dfbca85789
Herman <germanriv19@gmail.com>
2022-10-18 21:23:00 +0300

    Completed task.

--- EventsAndLINQ/EventsAndLINQ/Program.cs
+++ EventsAndLINQ/EventsAndLINQ/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using EventsAndLINQ;
+
+public class Program
 {
     public event Func<double, double, double> AddHandler;
 
@@ -6,6 +8,42 @@
     {
         var program = new Program();
         program.AddHandlerTransformarion(() => program.Add(2, 3));
+        var list = new List<Contact>() { new Contact("a", "1"), new Contact("b", "2"), new Contact("ca", "3") };
+        var listEmpty = new List<Contact>() { new Contact("b", "2"), new Contact("d", "4") };
+        Console.WriteLine(list.FirstOrDefault().Name);
+        Console.WriteLine();
+        var listWhere = list.Where(w => w.Name.Length < 2);
+        Console.WriteLine("Where");
+        foreach (var item in listWhere)
+        {
+            Console.WriteLine($"{item.Name} - {item.Number}");
+        }
+
+        Console.WriteLine();
+        var listSelect = list.Select(s => "2");
+        Console.WriteLine("Select");
+        foreach (var item in listSelect)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Any");
+        Console.WriteLine(list.Any(a => a.Name.Length > 3));
+        Console.WriteLine();
+        Console.WriteLine("Union");
+        var listUnion = list.Union(listEmpty);
+        foreach (var item in listUnion)
+        {
+            Console.WriteLine(item.Name);
+        }
+
+        list.Reverse();
+        Console.WriteLine("Reverse");
+        foreach (var item in list)
+        {
+            Console.WriteLine(item.Name);
+        }
     }
 
     public void AddHandlerTransformarion(Action action)
